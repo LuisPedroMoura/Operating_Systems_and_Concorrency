bf0781aff875366818bcfaacd81c2ffc75a391a5
Sérgio de Aguiar <sergio.aguiar@ua.pt>
2018-10-07 11:53:30 +0100

    Unfinished/Untested FILT

--- sofs18/src/work_src/work_mksofs/work_mksofs_FILT.cpp
+++ sofs18/src/work_src/work_mksofs/work_mksofs_FILT.cpp
@@ -17,10 +17,30 @@ namespace sofs18
             soProbe(603, "%s(%u, %u)\n", __FUNCTION__, first_block, itotal);
             
             /* change the following line by your code */
-            return bin::fillInFreeInodeListTable(first_block, itotal);
+            //return bin::fillInFreeInodeListTable(first_block, itotal);
+
+	    //Number of blocks needed for all the inodes
+	    uint32_t inodeBlocks = itotal / InodesPerBlock;
+
+	    //Since inodeBlocks is an integer number, we need to account for the mod (division remainder)
+	    if(itotal % InodesPerBlock != 0) inodeBlocks++;
+
+	    //Variable to assist in counting the first free block for inode storage
+	    uint32_t blockCount = first_block;
+	
+	    //Array that stores the inodes to write
+	    uint32_t inodeRL[InodesPerBlock];
+
+	    for(uint32_t i=0; i<inodeBlocks; i++) {
+  	    	for(uint32_t j=0; j<InodesPerBlock; j++) {
+		    inodeRL[j] = NullReference;
+		}
+		soWriteRawBlock(blockCount,&inodeRL);	
+		blockCount++;
+	    }
+
+	    return inodeBlocks;
         }
-
     };
-
 };
 
