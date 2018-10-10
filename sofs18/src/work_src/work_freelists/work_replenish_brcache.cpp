7db384f2ba79d1578b6f4093ceb0fd9af2199b04
Maria Joao Lavoura <mjlavoura@ua.pt>
2018-10-10 17:51:34 +0100

    replenish block retrieval cache

--- sofs18/src/work_src/work_freelists/work_replenish_brcache.cpp
+++ sofs18/src/work_src/work_freelists/work_replenish_brcache.cpp
@@ -25,8 +25,67 @@ namespace sofs18
         {
             soProbe(443, "%s()\n", __FUNCTION__);
 
+            SOSuperBlock *sb = soSBGetPointer();
+
+            SOBlockReferenceCache retrievalCache = sb->brcache;
+
+            if(retrievalCache.idx != BLOCK_REFERENCE_CACHE_SIZE ){
+            	return;
+            }
+
+            //get references from insertion cache
+            if(sb->fblt_head == sb->fblt_tail){
+            	SOBlockReferenceCache insertionCache = sb->bicache;
+            	uint32_t insertionIDX = insertionCache.idx;
+
+            	//insertion cache is empty
+            	if(insertionIDX==0){
+            		return ;
+            	}
+
+            	uint32_t destStart = BLOCK_REFERENCE_CACHE_SIZE - insertionIDX;
+            	memcpy(&(retrievalCache.ref[destStart]), insertionCache.ref, insertionIDX);
+
+            }
+            else{
+				//get references from fblt
+				uint32_t block = sb->fblt_head / ReferencesPerBlock;
+				uint32_t ref = sb->fblt_head % ReferencesPerBlock;
+
+				uint32_t *blockPointer = soFBLTOpenBlock(block);
+				uint32_t nRefAvailable = ReferencesPerBlock - ref;
+
+				if(nRefAvailable >= BLOCK_REFERENCE_CACHE_SIZE){
+
+					uint32_t *source = blockPointer + ref*sizeof(uint32_t);
+					memcpy(&retrievalCache, source, BLOCK_REFERENCE_CACHE_SIZE);
+
+					//update idx
+					retrievalCache.idx = 0;
+
+					//update fblt head
+					sb->fblt_head += BLOCK_REFERENCE_CACHE_SIZE;
+
+				}
+				else{
+
+					uint32_t *source = blockPointer + ref*sizeof(uint32_t);
+					uint32_t destStart = BLOCK_REFERENCE_CACHE_SIZE - nRefAvailable;
+					memcpy(&retrievalCache.ref[destStart], source, nRefAvailable);
+
+					//update idx
+					retrievalCache.idx = destStart;
+					//update fblt head
+					sb->fblt_head += nRefAvailable;
+
+				}
+            }
+
+            //soSBClose();
+            //soSBSave();
+
             /* change the following line by your code */
-            bin::soReplenishBRCache();
+            //bin::soReplenishBRCache();
         }
 
     };
