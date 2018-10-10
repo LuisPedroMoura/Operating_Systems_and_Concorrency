0f36976cba7e1bec68e70e715ea875bd1eda683f
Luis Moura <luisppmoura@gmail.com>
2018-10-10 17:49:50 +0100

    replenish ircache first functioning version

--- sofs18/src/work_src/work_freelists/work_replenish_ircache.cpp
+++ sofs18/src/work_src/work_freelists/work_replenish_ircache.cpp
@@ -26,7 +26,72 @@ namespace sofs18
             soProbe(403, "%s()\n", __FUNCTION__);
 
             /* change the following line by your code */
-            bin::soReplenishIRCache();
+            //bin::soReplenishIRCache();
+
+            printf("ESTOU NA MINHA FUNÇÃO - WEEEEEEEEEE!");
+
+            SOSuperBlock *sb = soSBGetPointer();
+
+            SOInodeReferenceCache cache = sb->ircache;
+
+            if (cache.idx != INODE_REFERENCE_CACHE_SIZE) {
+            	return;
+            }
+
+            if (sb->filt_tail == sb->filt_head) {
+
+            	SOInodeReferenceCache insertionCache = sb->iicache;
+            	uint32_t insertionIDX = insertionCache.idx;
+
+            	if (insertionIDX == 0){
+            		// all the disk is ocuppied
+            		// TODO verificar com prof o que fazer nesta situação
+            		return;
+            	}
+
+            	uint32_t destStart = INODE_REFERENCE_CACHE_SIZE - insertionIDX;
+            	memcpy(&(cache.ref[destStart]) , insertionCache.ref, insertionIDX);
+            	return;
+            }
+
+            else {
+				uint32_t block = sb->filt_head / ReferencesPerBlock;
+				uint32_t ref = sb->filt_head % ReferencesPerBlock;
+
+				uint32_t *blockPointer = soFILTOpenBlock(block);
+				uint32_t refsAvailable = ReferencesPerBlock - ref;
+
+				if ((refsAvailable) >= INODE_REFERENCE_CACHE_SIZE) {
+
+					// copy cache size chunk from filt to ircache
+					uint32_t *source = blockPointer += ref * sizeof(uint32_t);
+					memcpy(&cache, source, INODE_REFERENCE_CACHE_SIZE);
+
+					// update idx
+					cache.idx = 0;
+
+					// update filt head
+					sb->filt_head += INODE_REFERENCE_CACHE_SIZE;
+				}
+
+				else {
+
+					// copy
+					uint32_t *source = blockPointer + ref * sizeof(uint32_t);
+					uint32_t destStart = INODE_REFERENCE_CACHE_SIZE - refsAvailable;
+					memcpy(&cache.ref[destStart], source, refsAvailable);
+
+					// update idx
+					cache.idx = destStart;
+
+					// update filt head
+					sb->filt_head += refsAvailable;
+				}
+            }
+
+            //soSBClose();
+            //soSBSave();
+
         }
 
     };
