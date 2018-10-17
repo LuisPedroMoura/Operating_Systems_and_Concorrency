cf685564fde1760aebac82677fc4c88e840ba866
Fábio Alves <fabioalves0@ua.pt>
2018-10-17 17:45:16 +0100

    deplete_iicache correção de 1 erro

--- sofs18/src/work_src/work_freelists/work_deplete_iicache.cpp
+++ sofs18/src/work_src/work_freelists/work_deplete_iicache.cpp
@@ -30,74 +30,70 @@ namespace sofs18
 
             /* change the following line by your code */
             
-	    SOSuperBlock *sb = soSBGetPointer();
+            SOSuperBlock *sb = soSBGetPointer();
 	
-	    uint32_t block = sb-> filt_tail / ReferencesPerBlock ;
-	    uint32_t block_used_refs = sb-> filt_tail % ReferencesPerBlock;
-	    uint32_t *block_pointer = soFILTOpenBlock(block);	    
-	    uint32_t block_free_refs;
+            uint32_t block = sb-> filt_tail / ReferencesPerBlock ;
+            uint32_t block_used_refs = sb-> filt_tail % ReferencesPerBlock;
+            uint32_t *block_pointer = soFILTOpenBlock(block);
+            uint32_t block_free_refs;
 
-		if( block == sb-> filt_head / ReferencesPerBlock ){
-		
-		    if( (sb-> filt_head) % ReferencesPerBlock >  (sb-> filt_tail) % ReferencesPerBlock){		
-	            block_free_refs =  (sb-> filt_head) % ReferencesPerBlock -  (sb-> filt_tail) % ReferencesPerBlock ;
-		    }
-		    else{
-		    block_free_refs = ReferencesPerBlock - block_used_refs;
-		    }
-		
-		}
-		else{
-		
-		    block_free_refs = ReferencesPerBlock - block_used_refs;
+			if( block == sb-> filt_head / ReferencesPerBlock ){
 
-		}	
-
-
-		if( sb->iicache.idx > block_free_refs ){
-		
-			memcpy(&(block_pointer[block_used_refs]),&(sb->iicache),block_free_refs * sizeof(uint32_t));
+				if( (sb-> filt_head) % ReferencesPerBlock >  (sb-> filt_tail) % ReferencesPerBlock){
+					block_free_refs =  (sb-> filt_head) % ReferencesPerBlock -  (sb-> filt_tail) % ReferencesPerBlock ;
+				}
+				else{
+					block_free_refs = ReferencesPerBlock - block_used_refs ;
+				}
 			
-
-			if(  sb-> filt_size > block ){
-                        	sb->filt_tail += block_free_refs;
 			}
 			else{
-				sb->filt_tail = 0;
+
+				block_free_refs = ReferencesPerBlock - block_used_refs ;
+
 			}
 
-			memcpy(&(sb->iicache),&(sb->iicache.ref[block_free_refs]),((sb->iicache.idx)-block_free_refs)*sizeof(uint32_t));	
 
-                        sb->iicache.idx -= block_free_refs ;
+			if( sb->iicache.idx > block_free_refs ){
 
+				memcpy(&(block_pointer[block_used_refs]),&(sb->iicache),block_free_refs * sizeof(uint32_t));
+
+
+				if(  (sb-> filt_size)-1 > block ){
+					sb->filt_tail += block_free_refs;
+				}
+				else{
+					sb->filt_tail = 0;
+				}
+
+				memcpy(&(sb->iicache),&(sb->iicache.ref[block_free_refs]),((sb->iicache.idx)-block_free_refs)*sizeof(uint32_t));
+
+					sb->iicache.idx -= block_free_refs ;
+
+
+				for(uint32_t i= 0 ; i < block_free_refs ; i++ ){
+
+					sb->iicache.ref[(sb->iicache.idx)+i] = NullReference;
+
+				}
 			
-			for(uint32_t i= 0 ; i < block_free_refs ; i++ ){
+			}
 
-                                sb->iicache.ref[(sb->iicache.idx)+i] = NullReference;
-
-                        }
-
-
-
-
-		}
-
+			else{
 		
-		else{
-		
-			memcpy(&(block_pointer[block_used_refs]),&(sb->iicache),(sb->iicache.idx)*sizeof(uint32_t));
+				memcpy(&(block_pointer[block_used_refs]),&(sb->iicache),(sb->iicache.idx)*sizeof(uint32_t));
 	
         		sb->filt_tail += sb->iicache.idx;
 			
-			sb->iicache.idx = 0;
+        		sb->iicache.idx = 0;
 
-			for(uint32_t i=0 ; i < INODE_REFERENCE_CACHE_SIZE ; i++ ){
+        		for( uint32_t i=0 ; i < INODE_REFERENCE_CACHE_SIZE ; i++ ){
 
-				sb->iicache.ref[i] = NullReference;
+        			sb->iicache.ref[i] = NullReference;
 			
-			}	
+        		}
 		
-		}
+			}
 
 		soFILTSaveBlock();
 		soFILTCloseBlock();
