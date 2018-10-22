9a8d8146217a528358e477ed3217b94cdb96bcd0
Sérgio de Aguiar <sergio.aguiar@ua.pt>
2018-10-22 19:11:14 +0100

    Alloc Fileblock base architecture

--- sofs18/src/work_src/work_fileblocks/work_alloc_fileblock.cpp
+++ sofs18/src/work_src/work_fileblocks/work_alloc_fileblock.cpp
@@ -26,7 +26,42 @@ namespace sofs18
             soProbe(302, "%s(%d, %u)\n", __FUNCTION__, ih, fbn);
 
             /* change the following two lines by your code */
-            return bin::soAllocFileBlock(ih, fbn);
+            //return bin::soAllocFileBlock(ih, fbn);
+
+	    SOInode* in = soITGetInodePointer(ih);
+
+	    //if index beyond d[]
+	    if(fbn > N_DIRECT) {
+		//if index beyond i1[0]
+		if(fbn > ReferencesPerBlock + N_DIRECT) {
+		    //if index beyond i1[1]
+		    if(fbn > (2 * ReferencesPerBlock) + N_DIRECT) {
+			//if index beyond i2[0]
+			if(fbn > (ReferencesPerBlock * ReferencesPerBlock) + (2 * ReferencesPerBlock) + N_DIRECT) {			    
+			    //add to i2[1]
+			    soAllocDoubleIndirectFileBlock( , fbn - ((ReferencesPerBlock * ReferencesPerBlock) + (2 * ReferencesPerBlock) + N_DIRECT) );
+			else {
+ 			    //add to i2[0]
+			    soAllocDoubleIndirectFileBlock( , fbn - ((2 * ReferencesPerBlock) + N_DIRECT) );
+			}
+		    }
+		    else {
+			//add to i1[1]
+			soAllocIndirectFileBlock( , fbn - (ReferencesPerBlock + N_DIRECT) );
+		    }
+		}
+		else {
+ 		    //add to i1[0]
+		    soAllocIndirectFileBlock( , fbn - N_DIRECT );
+		}
+	    }
+	    else {
+		//add to d[]
+		in->d[fbn] = ;
+	    }
+
+	    //return number of allocated block
+	    return ... ; 
         }
 
         /* ********************************************************* */
