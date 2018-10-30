c5ded75ced55f0a76fda768607f5d7fcd27f7065
Ricardo Rodrigues Azevedo <azevedoricardo98@ua.pt>
2018-10-30 22:26:05 +0000

    soRenameDirentry finalizado

--- sofs18/src/work_src/work_direntries/work_rename_direntry.cpp
+++ sofs18/src/work_src/work_direntries/work_rename_direntry.cpp
@@ -24,44 +24,46 @@ namespace sofs18
 	    SOInode* in = soITGetInodePointer(pih);
 	    SOSuperBlock *sb = soSBGetPointer();
 	   
-	    if(pih>sb->itotal)
+	    if(pih>(sb->itotal))
 		throw SOException(EINVAL,__FUNCTION__);
 
 	    if (!S_ISDIR(in->mode))
 		throw SOException(ENOTDIR,__FUNCTION__);
 
 	    uint32_t blocks = in->blkcnt; //Number of block use by the file
-	    //uint32_t maxBlocks = N_DIRECT + (DirentriesPerBlock*N_INDIRECT) + (DirentriesPerBlock*DirentriesPerBlock)*N_DOUBLE_INDIRECT;
-	    uint32_t* arrp; //pointer of the array d[],i1[],i2[]
+	    
 	    SODirEntry buff[ReferencesPerBlock];
-	    
-	    arrp = in->d;
-	    
+	   
 	    SODirEntry dir;
 
-	    uint32_t i = 0;
+	    uint32_t index = 0;
+	    uint32_t blockn = 0;
+	    uint32_t blockcnt = 0;
 	    uint32_t j = 0;
-	    while(i<blocks){
-		if(j>=N_DIRECT) arrp=in->i1;
-		if(j>=N_DIRECT + (DirentriesPerBlock*N_INDIRECT)) arrp=in->i2;
-		if(j>=N_DIRECT + (DirentriesPerBlock*N_INDIRECT) + (DirentriesPerBlock*DirentriesPerBlock)*N_DOUBLE_INDIRECT) i=blocks;
-
-		if(arrp[j]!=NullReference){
-			sofs18::soReadFileBlock(pih,arrp[j],buff);
-			for(uint32_t t=0;t<ReferencesPerBlock;t++){
-				dir = buff[t];
+	    uint32_t check = 1;
+	    while(blockcnt<blocks){
+		blockn = sofs18::soGetFileBlock(pih,index);
+		if(blockn==NullReference){
+			sofs18::soReadFileBlock(pih,index,buff);
+			for(j=0;j<ReferencesPerBlock;j++){
+				dir = buff[j];
 				if(strcmp(dir.name,name)==0){
 					memmove(dir.name,newName,SOFS18_MAX_NAME+1);
-					i=blocks;
-					buff[t]=dir;
-					sofs18::soWriteFileBlock(pih,arrp[j],buff);
+					blockcnt=blocks-1;
+					check = 0;
+					buff[j]=dir;
+					sofs18::soWriteFileBlock(pih,index,buff);
 				}
 			}
-			i+=1;
+			blockcnt+=1;
 		}
 		else
 			j+=1;
-	    }        
+		index+=1;
+	    }
+	
+	    if(check!=0)
+		throw SOException(ENOENT,__FUNCTION__);       
 	}
 
     };
