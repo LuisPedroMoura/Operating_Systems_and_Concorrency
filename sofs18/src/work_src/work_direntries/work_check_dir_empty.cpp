b2ba1c2098da1e5f24db832a7c19cd1e03b36928
Sérgio de Aguiar <sergio.aguiar@ua.pt>
2018-10-31 13:01:43 +0000

    Check Dir Empty done and slightly tested, more tests run later

--- sofs18/src/work_src/work_direntries/work_check_dir_empty.cpp
+++ sofs18/src/work_src/work_direntries/work_check_dir_empty.cpp
@@ -19,7 +19,35 @@ namespace sofs18
             soProbe(205, "%s(%d)\n", __FUNCTION__, ih);
 
             /* change the following line by your code */
-            return bin::soCheckDirEmpty(ih);
+            //"return bin::soCheckDirEmpty(ih);
+	
+	    //Reference to the node being handled
+	    SOInode* node = soITGetInodePointer(ih);
+	    
+	    uint32_t blk = (node->size)/BlockSize;
+	    SODirEntry buff[DirentriesPerBlock];
+
+	    uint32_t blkCount = 0;
+	    uint32_t blkNum = 0;
+	    uint32_t tmpBlk;
+
+	    while(blkCount < blk) {
+		tmpBlk = sofs18::soGetFileBlock(ih,blkNum);
+
+		if(tmpBlk != NullReference) {
+		    sofs18::soReadFileBlock(ih,blkNum,buff);
+		
+		    for(uint32_t i = 2; i < DirentriesPerBlock; i++) {
+		    	if(strcmp(buff[i].name,"\0") != 0) return false;
+	    	    }
+
+		    blkCount++;
+		}
+
+		blkNum++;
+	    }
+
+	    return true; 
         }
 
     };
